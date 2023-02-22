edc9db1694e69014c26ca49a55ae4910cfb0b04f
Josh Famestad <famestad@amazon.com>
2023-02-22 07:49:12 -0800

    debugging - working - todo: narrow network access for processing sg

--- VideoProcessing/functions/check_rek_results/app.py
+++ VideoProcessing/functions/check_rek_results/app.py
@@ -30,11 +30,13 @@ def lambda_handler(event, context):
     response_data = json.dumps(response)
 
     key = f"video_labels/{job_id}.json"
+    print(f"writing rek response to {key}")
     s3.put_object(
         Bucket=graph_load_processing_bucket,
         Key=key,
         Body=response_data,
     )
+    print(f"done writing rek response to s3")
 
     event["LabelDetectionComplete"] = response["JobStatus"]
     event["LabelDataS3Key"] = key
